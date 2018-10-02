bcce6a9839cbf39f32873d298ec72b7431c416b0
Galilea América Loretto Estrada <Bucketh@users.noreply.github.com>
2018-10-02 09:57:13 -0500

    Asignacion de pines

--- RegistroUniversal8/regUniversal.vhd
+++ RegistroUniversal8/regUniversal.vhd
@@ -8,6 +8,29 @@ PORT(
 		DATO: IN STD_LOGIC_VECTOR(7 DOWNTO 0);
 		Q: INOUT STD_LOGIC_VECTOR(7 DOWNTO 0)
 	);
+ATTRIBUTE PIN_NUMBERS OF ABCD : ENTITY IS
+        "Q(0):23 " &   
+        "Q(1):22 " &   
+        "Q(2):21 " &   
+        "Q(3):20 " &   
+        "Q(4):19 " &   
+        "Q(5):18 " &   
+        "Q(6):17 " &
+	"Q(7):16 " & 
+	"DATO(0):15 " &
+        "DATO(1):14 " &
+	"DATO(2):13 " &
+        "DATO(3):11 " &
+        "DATO(4):10 "  &
+        "DATO(5):9 " &
+        "DATO(6):8 " &
+        "DATO(7):7 " &
+	"C(0):6 " &
+        "C(1):5 " &
+        "CI:4 " &   
+        "CD:3 " &   
+        "CLR:2 " &   
+        "CLK:1"; 
 END ENTITY;
 
 ARCHITECTURE A_RU8 OF RU8 IS
